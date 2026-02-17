c6ed5d25c1cc7c7981dd50f627bda395832ca2b8
Xuemeiat <justbehappy37@gmail.com>
2026-02-17 14:38:58 +0400

    add schema and documentation

--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
 from .models import Post
-from rest_framework import generics
-from .permissions import IsAuthorOrReadOnly #new
-from .serializers import PostSerializer
+from rest_framework import viewsets #new
+from rest_framework.permissions import IsAdminUser #new
+from .permissions import IsAuthorOrReadOnly 
+from .serializers import PostSerializer, UserSerializer
 
-class PostList(generics.ListCreateAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)# new
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthorOrReadOnly,)#new
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminUser]
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
